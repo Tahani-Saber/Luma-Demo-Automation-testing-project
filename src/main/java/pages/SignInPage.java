170e18c5e4ac161b83208ffc1f2386233e48e595
Tahani_Habib <138535022+Tahani-Saber@users.noreply.github.com>
2024-03-28 07:35:33 +0200

    Upload Sign In Page
    sign in feature of the Luma

---
+++ src/main/java/pages/SignInPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignInPage {
+    WebDriver w;
+    public  SignInPage(WebDriver W){
+        this.w =W;
+    }
+    By EmailField = By.xpath("//*[@id=\"email\"]");
+    By PasswordField = By.xpath("//*[@id=\"pass\"]");
+    By SignInButton = By.xpath("//*[@id=\"send2\"]");
+
+    public void enterEmail(String Email) {
+        w.findElement(EmailField).sendKeys(Email);
+    }
+    public void enterPassword(String Password) {
+        w.findElement(PasswordField).sendKeys(Password);
+    }
+    public void SignInButton() {
+        w.findElement(SignInButton).click();
+    }
+}
